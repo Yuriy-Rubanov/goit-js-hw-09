b0365880a1588bef8bad393bcdac8d23be9f6bd2
Yuriy Rubanov <yrubanov@gmail.com>
2024-06-24 13:47:21 +0300

    correct filenames

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -15,7 +15,6 @@ function populateForm(data) {
   form.elements['message'].value = data.message;
 }
 
-// Делегування події input для зберігання даних у локальному сховищі
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', function (e) {
   if (e.target.matches('input[type="email"]')) {
@@ -24,27 +23,21 @@ form.addEventListener('input', function (e) {
     formData.message = e.target.value;
   }
 
-  // Збереження formData у локальному сховищі
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
 
-// Обробка відправлення форми
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  // Перевірка заповненості полів
   if (formData.email.trim() === '' || formData.message.trim() === '') {
     alert('Fill please all fields');
     return;
   }
 
-  // Вивід formData у консоль
   console.log(formData);
 
-  // Очищення локального сховища та об’єкта formData
   localStorage.removeItem('feedback-form-state');
   formData = { email: '', message: '' };
 
-  // Очищення полів форми
   form.reset();
 });
